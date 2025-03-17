9413a0aa435ab8cfce6ae8bfdcf03a96c617b4f7
Jacqueline <jacquelinenyinawabagesera@gmail.com>
2025-03-17 19:20:31 +0300

    extract the last four characters

---
+++ index.js
@@ -0,0 +1,5 @@
+//1. Extract the last four characters from the string below;"extravaganza"
+
+const str = "extravaganza";
+const lastFourCharacters = str.slice(-4);
+console.log(lastFourCharacters);
