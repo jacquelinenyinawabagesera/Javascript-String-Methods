ac2b25d7f3cadff461c695e7bbf097281345c4c8
Jacqueline <jacquelinenyinawabagesera@gmail.com>
2025-03-17 19:45:55 +0300

    added a string to another string

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-//1. Extract the last four characters from the string below;"extravaganza"
+// 1. Extract the last four characters from the string below;"extravaganza"
 
 const str = "extravaganza";
 const lastFourCharacters = str.slice(-4);
 console.log(lastFourCharacters);
+
+// 2. Insert the following string at the fourth index of the below variable:"eat"const food = "The quick fox jumped over the lazy dog"
+
+const food = "The quick fox jumped over the lazy dog";
+const resultString = food.slice(0,4) + "eat " + food.slice(4);
+console.log(resultString);
