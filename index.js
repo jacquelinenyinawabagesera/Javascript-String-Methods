bf44026f360e1ee5a4ed7aa0d87029871394b0ab
Jacqueline <jacquelinenyinawabagesera@gmail.com>
2025-03-19 13:11:39 +0300

    answered number three

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ console.log(lastFourCharacters);
 // 2. Insert the following string at the fourth index of the below variable:"eat"const food = "The quick fox jumped over the lazy dog"
 
 const food = "The quick fox jumped over the lazy dog";
-const resultString = food.slice(0,4) + "eat " + food.slice(4);
+const resultString = food.slice(0,4) + "eat" + " " + food.slice(4);
 console.log(resultString);
 
 // 3. Count how many times the following string appears in the string variable:1. "the"2."brown"const story= "The quick brown fox jumps over the lazy dog"
@@ -47,8 +47,8 @@ console.log(str2.toLowerCase());
 const str3 = "UndERneath";
 console.log(str3.toLowerCase());
 
-const inputString = "a wonderful world";
-const wordsList = inputString.split(' ');
-const outputText = wordsList.map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(' ');
+const originalString = "a wonderful world";
+const textWords = originalString.split(' ');
+const newString = textWords.map(text => text.charAt(0).toUpperCase() + text.slice(1).toLowerCase()).join(' ');
 
-console.log(outputText);
+console.log(newString);
