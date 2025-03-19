9bf158ab686eefac294cafe8c2952311f5ee689f
Jacqueline <jacquelinenyinawabagesera@gmail.com>
2025-03-19 13:03:55 +0300

    answered number two

--- index.js
+++ index.js
@@ -9,3 +9,46 @@ console.log(lastFourCharacters);
 const food = "The quick fox jumped over the lazy dog";
 const resultString = food.slice(0,4) + "eat " + food.slice(4);
 console.log(resultString);
+
+// 3. Count how many times the following string appears in the string variable:1. "the"2."brown"const story= "The quick brown fox jumps over the lazy dog"
+const story = "The quick brown fox jumps over the lazy dog";
+const countWords = story.split(" ")
+
+let countThe = 0;
+let countBrown = 0;
+
+countWords.forEach(word => {
+    if (word === "the"){
+        countThe++
+    }
+    else if(word === "brown"){
+        countBrown++
+    }
+});
+console.log(` the appears ${countThe} times`);
+console.log(`brown appears ${countBrown} times`);
+
+
+//4.Using JavaScript, find the following words from the following strings:1. "are"const string1 = "The pupils are reading in the library";2. "sitting"const string2 = "The child was sitting on the table before it fell"
+const string1 = "The pupils are reading in the library";
+const string2 = "The child was sitting on the table before it fell";
+const areIncluded = string1.includes('are');
+console.log(areIncluded);
+const sittingIncluded = string2.includes('sitting');
+console.log(sittingIncluded);
+
+
+//5.Convert the following strings into the specified format:1. UpperCase: "wonderful"2. LowerCase: "amazing", "UndERneath"3. Title case "A wonderful world"
+
+const str1 = "wonderful";
+console.log(str1.toUpperCase());
+const str2 = "amazing";
+console.log(str2.toLowerCase());
+const str3 = "UndERneath";
+console.log(str3.toLowerCase());
+
+const inputString = "a wonderful world";
+const wordsList = inputString.split(' ');
+const outputText = wordsList.map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(' ');
+
+console.log(outputText);
